2c18954844be1d5120f4062a6d04d163d1737d45
vupn <vupham140199@gmail.com>
2023-01-12 09:55:51 +0700

    completed

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,17 @@
-const ValidationError = require('./validation-error');
+const ValidationError = require("./validation-error");
 
 // Your code here
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength) {
+    super(excessLength);
+    this.name = "MaximumLengthExceededError";
+    if (excessLength) {
+      this.message = `Maximum length exceeded by ${excessLength}`;
+    } else {
+      this.message = `Maximum length exceeded`;
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
